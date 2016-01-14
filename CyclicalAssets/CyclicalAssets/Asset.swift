9b22f2252f68fd6c1b6fafd16da8ca7ee3668471
kernjackson <kernjackson@gmail.com>
2016-01-14 16:49:33 -0600

    No idea why .DS_Store and .swp won't be ignored properly

---
+++ CyclicalAssets/CyclicalAssets/Asset.swift
@@ -0,0 +1,9 @@
+//
+//  Asset.swift
+//  CyclicalAssets
+//
+//  Created by admin on 1/12/16.
+//  Copyright © 2016 Kern. All rights reserved.
+//
+
+import Foundation
---
+++ CyclicalAssets/CyclicalAssets/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  CyclicalAssets
+//
+//  Created by admin on 1/12/16.
+//  Copyright © 2016 Kern. All rights reserved.
+//
+
+import Foundation
